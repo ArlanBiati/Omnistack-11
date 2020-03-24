01607fed675e1120879d41896e4c0668fda38fbc
ArlanBiati <arlangustavo@hotmail.com>
2020-03-24 18:05:16 -0300

    Chore: Finalizado Back-end

---
+++ backend/src/controllers/ProfileController.js
@@ -0,0 +1,13 @@
+const connections = require('../database/connection')
+
+module.exports = {
+  async index(req, res) {
+    const ong_id = req.headers.autorization;
+
+    const incident = await connections('incidents')
+      .where('ong_id', ong_id)
+      .select('*')
+
+    return res.json(incident)
+  }
+}
